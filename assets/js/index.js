708f54a6a134d5a0c6b65dbe8366f4d1753fdf98
americagit <meri.soto.campos@outlook.com>
2024-02-20 10:18:30 -0300

    primera version de ricomida

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $('#enviarCorreo').click(function(){
+        alert('El correo fue enviado correctamente...');
+    });
+
+    $('#titulo h3').on('dblclick',function(){
+        $(this).addClass('text-danger');
+    });
+
+    $('#recetasRelacionadas .card-title').on('click',function(){
+        $('.card-text').toggle(2000);
+    });
+})
